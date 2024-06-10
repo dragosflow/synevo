4c79f72393c80319203a1b2bd8270eb75bb30b6d
dragos <dragosflow@yahoo.com>
2024-06-10 19:05:27 +0300

    chore: create CircleButton component

---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  onClick?: () => void;
+  className?: string;
+  icon: React.ReactNode;
+}
+export const Button = ({ onClick, className, icon }: ButtonProps) => {
+  return (
+    <button
+      onClick={() => {
+        onClick && onClick();
+      }}
+      className={`shadow-md p-0.5 font-bold rounded-full shadow-gray-300 border-[1px] border-slate-100/40 ${className}`}
+    >
+      {icon}
+    </button>
+  );
+};

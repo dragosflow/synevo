c9be02fa1fe96df8d80b4b602ff4ad5cd6aeb4ed
dragos <dragosflow@yahoo.com>
2024-06-11 01:44:52 +0300

    chore: add appointment list component

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Options } from "./components/Options";
 import { UserCard } from "./components/UserCard";
-import { TestCard } from "./components/TestCard";
 import user from "./assets/svgs/persons/user.svg";
 import { TestList } from "./components/TestList";
+import { SectionTitle } from "./components/SectionTitle";
+import { AppointmentList } from "./components/AppointmentList";
 
 function App() {
   return (
-    <div className='w-full h-screen bg-synevo-gray'>
+    <div className='w-full h-full bg-synevo-gray'>
       <Header />
       <Options />
       <UserCard name='Elisabeta Aardeleanu' photoSrc={user} />
       <TestList />
+      <SectionTitle
+        title='Appointments'
+        PlusButton={{ show: true }}
+        ChevronRightButton={{ show: true }}
+      />
+      <AppointmentList />
     </div>
   );
 }

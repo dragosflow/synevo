265aa6764cc3acb1fff9d0372150e2848e6d65fe
dragos <dragosflow@yahoo.com>
2024-06-10 20:43:13 +0300

    chore: create ScrollableContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Header } from "./components/Header";
-
+import { OptionsCard } from "./components/OptionsCard";
+import optionsMedicalIcon from "./assets/svgs/options-medical-icon.svg";
+import ScrollableContainer from "./components/ScrollableContainer";
 function App() {
   return (
     <div className='w-full h-screen bg-synevo-gray'>

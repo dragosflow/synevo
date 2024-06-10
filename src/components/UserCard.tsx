e9331d7fcbbb70a9ab7c84a4c5199e4c0a0f90c8
dragos <dragosflow@yahoo.com>
2024-06-10 23:56:03 +0300

    fix: add props to UserCard

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import user from "../assets/svgs/persons/user.svg";
 import { CircleButton } from "./CircleButton";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
-export const UserCard = () => {
+export const UserCard = ({
+  name,
+  photoSrc,
+}: {
+  name: string;
+  photoSrc: string;
+}) => {
   return (
     <div className='flex pl-6 mt-3 w-full'>
-      <img src={user} alt='user' className='w-12 h-12' />
+      <img src={photoSrc} alt='user' className='w-12 h-12' />
       <div className='ml-2 my-auto w-full'>
         <p className='font-semibold text-lg  tracking-wide'>
           Latest Results for
         </p>
-        <p className='font-medium -mt-2 text-sm text-synevo-blue'>
-          Elisabeta Aardeleanu
-        </p>
+        <p className='font-medium -mt-2 text-sm text-synevo-blue'>{name}</p>
       </div>
       <div className='w-max justify-end my-auto pr-6'>
         <CircleButton
